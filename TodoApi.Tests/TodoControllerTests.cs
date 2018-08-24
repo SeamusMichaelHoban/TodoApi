ad3d96d006b9a7b2c2e17c3a34054a3258101787
SeamusMichaelHoban <seamushoban2000@gmail.com>
2018-08-23 22:16:42 -0400

    first commit

---
+++ TodoApi.Tests/TodoControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApi.Controllers;
+using TodoApi.Models;
+
+namespace TodoApi.Tests
+{
+    [TestFixture]
+    public class TodoControllerTests
+    {
+            [Test]
+            public void GetAll_Returns_A_List_Of_Todos()
+            {
+                var controller = new TodoController();
+                var result = controller.GetAll();
+                Assert.That(result.Count, Is.EqualTo(1));
+            }
+            
+            [Test]
+            public void Post_Saves_Todo()
+        {
+            var todo = new Todo();
+            var controller = new TodoController();
+
+            controller.Post(todo);
+
+            var result = controller.GetAll();
+
+            Assert.That(result.Count, Is.EqualTo(2));
+        }
+
+    }
+}
---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    public class TodoController : ApiController
+    {
+        private static List<Todo> todos = new List<Todo>()
+            {
+                new Todo() { Id = 1, Name = "Feed cat", Priority = 1}
+            };
+        public List<Todo> GetAll()
+        {
+            return todos;
+        }
+
+        public void Post(Todo todo)
+        {
+            todos.Add(todo);
+        }
+    }
+}
